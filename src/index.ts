4222cd7dd5aaa7688333204045eda90bcbd0192a
Leynier Gutiérrez González <leynier41@gmail.com>
2024-11-23 21:21:44 -0600

    chore: split the implementation in some functions with specific responsibility

--- src/index.ts
+++ src/index.ts
@@ -65,8 +65,16 @@ export function useAsyncDataWrapper<T extends Record<string, any>>(
   obj: T,
 ): AsyncDataWrapper<T> {
   const composable = {} as AsyncDataWrapper<T>;
+  const functionNames = getFunctionNames(obj);
+  for (const functionName of functionNames) {
+    const originalFunction = obj[functionName].bind(obj);
+    const wrappedFunction = getWrappedFunction(functionName, originalFunction);
+    (composable as any)[functionName] = wrappedFunction;
+  }
+  return composable;
+}
 
-  // Get all function names from the object and its prototypes
+function getFunctionNames(obj: any): string[] {
   const functionNameSet = new Set<string>();
   let currentObj = obj;
   while (currentObj && currentObj !== Object.prototype) {
@@ -79,72 +87,82 @@ export function useAsyncDataWrapper<T extends Record<string, any>>(
     }
     currentObj = Object.getPrototypeOf(currentObj);
   }
-  const functionNames = Array.from(functionNameSet);
+  return Array.from(functionNameSet);
+}
 
-  for (const key of functionNames) {
-    // Bind the function to preserve context
-    const originalFunction = obj[key].bind(obj);
+function getWrappedFunction(
+  functionName: string,
+  originalFunction: (...args: any[]) => Promise<any>,
+): (...args: any[]) => AsyncDataResult<Promise<any>> {
+  return (...args: any[]) => {
+    if (typeof args[0] === 'function') {
+      // Function with arguments: first argument is argsSupplier, second is options
+      const argsSupplier = args[0];
+      const options = args[1];
+      return getWrappedFunctionWithArgs(
+        functionName,
+        originalFunction,
+        argsSupplier,
+        options!,
+      );
+    }
+    // Function without arguments: options may be the first argument
+    const options = args[0];
+    return getWrapedFunctionWithoutArgs(
+      functionName,
+      originalFunction,
+      options!,
+    );
+  };
+}
 
-    // Create the wrapped function
-    (composable as any)[key] = ((...args: any[]) => {
-      let argsSupplier: (() => any[]) | undefined;
-      let options: AsyncDataOptions<any, any, any, any> | undefined;
+function getWrappedFunctionWithArgs<T extends (...args: any[]) => Promise<any>>(
+  functionName: string,
+  originalFunction: T,
+  argsSupplier: () => Parameters<T>,
+  options: AsyncDataOptions<ReturnType<T>>,
+): AsyncDataResult<ReturnType<T>> {
+  // Reactive reference to arguments
+  const argsRef = computed(() => argsSupplier!());
+  // Unique key for useAsyncData
+  const dataKeyRef = computed(
+    () => `${functionName}-${JSON.stringify(argsRef.value)}`,
+  );
+  // Call useAsyncData with the generated key and function
+  const asyncDataResult = useAsyncData(
+    dataKeyRef.value,
+    () => {
+      const result = originalFunction(...unref(argsRef));
+      // Ensure we return a Promise
+      return result instanceof Promise ? result : Promise.resolve(result);
+    },
+    {
+      // Re-execute when arguments change
+      watch: [argsRef],
+      // Spread additional options
+      ...options,
+    },
+  );
+  return asyncDataResult;
+}
 
-      if (typeof args[0] === 'function') {
-        // Function with arguments: first argument is argsSupplier, second is options
-        argsSupplier = args[0];
-        options = args[1];
-      } else {
-        // Function without arguments: options may be the first argument
-        options = args[0];
-      }
-
-      if (argsSupplier) {
-        // For functions with arguments
-
-        // Reactive reference to arguments
-        const argsRef = computed(() => argsSupplier!());
-        // Unique key for useAsyncData
-        const dataKeyRef = computed(
-          () => `${key}-${JSON.stringify(argsRef.value)}`,
-        );
-
-        // Call useAsyncData with the generated key and function
-        const asyncDataResult = useAsyncData(
-          dataKeyRef.value,
-          () => {
-            const result = originalFunction(...unref(argsRef));
-            // Ensure we return a Promise
-            return result instanceof Promise ? result : Promise.resolve(result);
-          },
-          {
-            // Re-execute when arguments change
-            watch: [argsRef],
-            // Spread additional options
-            ...options,
-          },
-        );
-
-        return asyncDataResult;
-      } else {
-        // For functions without arguments
-        const asyncDataResult = useAsyncData(
-          key,
-          () => {
-            const result = originalFunction();
-            // Ensure we return a Promise
-            return result instanceof Promise ? result : Promise.resolve(result);
-          },
-          {
-            // Spread additional options
-            ...options,
-          },
-        );
-
-        return asyncDataResult;
-      }
-    }) as any;
-  }
-
-  return composable;
+function getWrapedFunctionWithoutArgs<T extends () => Promise<any>>(
+  functionName: string,
+  originalFunction: T,
+  options: AsyncDataOptions<ReturnType<T>>,
+): AsyncDataResult<ReturnType<T>> {
+  // For functions without arguments
+  const asyncDataResult = useAsyncData(
+    functionName,
+    () => {
+      const result = originalFunction();
+      // Ensure we return a Promise
+      return result instanceof Promise ? result : Promise.resolve(result);
+    },
+    {
+      // Spread additional options
+      ...options,
+    },
+  );
+  return asyncDataResult;
 }
